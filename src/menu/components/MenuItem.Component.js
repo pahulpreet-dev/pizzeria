358e2e388d64cf061b0c3e7e86b984af4b694474
Harman Singh <pahul0957@gmail.com>
2021-07-12 17:18:52 -0400

    order modal as a component

--- src/menu/components/MenuItem.Component.js
+++ src/menu/components/MenuItem.Component.js
@@ -6,7 +6,7 @@ import NavbarMenu from "../../shared/Components/NavbarMenu.Component";
 import Avatar from "../../shared/UIElements/Avatar";
 
 import "./MenuItem.Component.css";
-import { Button, ButtonGroup, Modal } from "react-bootstrap";
+import OrderModal from "./OrderModal.component";
 
 const MenuItem = () => {
   const DUMMY_PIZZAS = [
@@ -57,14 +57,6 @@ const MenuItem = () => {
   //Modal
   const [show, setShow] = useState(false);
   const [modalPizza, setModalPizza] = useState();
-  const [price, setPrice] = useState(0.0);
-  const [disableQuantityButton, setDisableQuantityButton] = useState(true);
-  const [quantity, setQuantity] = useState(1);
-  const [validOrder, setValidOrder] = useState(false);
-  const [showError, setShowError] = useState(false);
-  const [cartPrice, setCartPrice] = useState(0.0);
-  const [pizzaSize, setPizzaSize] = useState();
-  const [checked, setChecked] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = (pizzaId_) => {
@@ -104,121 +96,12 @@ const MenuItem = () => {
           ))}
         </div>
       </div>
-      <Modal centered show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{modalPizza && modalPizza[0].title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="d-flex justify-content-center">
-          <div className="container">
-            <div className="row">
-              <ButtonGroup size="lg" className="m-2 btn-block">
-                <Button
-                  variant="outline-primary"
-                  className="m-2 btn-block"
-                  active={pizzaSize === "sm"}
-                  onClick={() => {
-                    setCartPrice(9.99);
-                    setPrice(9.99);
-                    setQuantity(1);
-                    setValidOrder(true);
-                    setShowError(false);
-                    setDisableQuantityButton(false);
-                    setPizzaSize("sm");
-                  }}
-                >
-                  Small
-                </Button>
-                <Button
-                  variant="outline-primary"
-                  className="m-2 btn-block"
-                  active={pizzaSize === "md"}
-                  onClick={() => {
-                    setCartPrice(11.99);
-                    setPrice(11.99);
-                    setQuantity(1);
-                    setValidOrder(true);
-                    setShowError(false);
-                    setDisableQuantityButton(false);
-                    setPizzaSize("md");
-                  }}
-                >
-                  Medium
-                </Button>
-                <Button
-                  variant="outline-primary"
-                  className="m-2 btn-block"
-                  active={pizzaSize === "lg"}
-                  onClick={() => {
-                    setCartPrice(13.99);
-                    setPrice(13.99);
-                    setQuantity(1);
-                    setValidOrder(true);
-                    setShowError(false);
-                    setDisableQuantityButton(false);
-                    setPizzaSize("lg");
-                  }}
-                >
-                  Large
-                </Button>
-              </ButtonGroup>
-            </div>
-            <div className="row d-flex justify-content-center mt-4">
-              Select quantity:
-            </div>
-            <div className="row d-flex justify-content-center">
-              <Button
-                disabled={disableQuantityButton}
-                variant="outline-danger"
-                className="m-1"
-                onClick={() => {
-                  quantity < 2 ? setQuantity(1) : setQuantity(quantity - 1);
-                  quantity < 2
-                    ? setCartPrice(price)
-                    : setCartPrice(cartPrice - price);
-                }}
-              >
-                <i class="fas fa-minus"></i>
-              </Button>
-              <h5 className="mt-2 mr-2 ml-2">{quantity}</h5>
-              <Button
-                disabled={disableQuantityButton}
-                variant="outline-success"
-                className="m-1"
-                onClick={() => {
-                  setQuantity(quantity + 1);
-                  setCartPrice(cartPrice + price);
-                }}
-              >
-                <i class="fas fa-plus"></i>
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer className="modal-footer">
-          <div className="container">
-            <div className="row d-flex flex-row-reverse">
-              <Button
-                onClick={() => {
-                  validOrder ? setShowError(false) : setShowError(true);
-                  validOrder &&
-                    console.log(` Cart details: \n Pizza Size: ${pizzaSize}\n Price: ${price}
-                  \n Quantity: ${quantity} \n Total: ${cartPrice.toFixed(2)}`);
-                }}
-              >
-                Add to Order
-              </Button>
-              <h5 className="mt-2 mr-2">${cartPrice.toFixed(2)}</h5>
-            </div>
-            <div className="row d-flex flex-row-reverse">
-              {showError && (
-                <span className="invalid-feedback d-block d-flex flex-row-reverse">
-                  Select size
-                </span>
-              )}
-            </div>
-          </div>
-        </Modal.Footer>
-      </Modal>
+      <OrderModal
+        show={show}
+        modalPizza={modalPizza}
+        handleClose={handleClose}
+      />
+      ;
     </div>
   );
 };
