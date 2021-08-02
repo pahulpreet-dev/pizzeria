d0c21a98a36e190d195f9e155d3f23fb5e5be9f1
Harman Singh <pahul0957@gmail.com>
2021-08-02 11:57:06 -0400

    connected signup api with frontend

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ mongoose
   .then(() => console.log("MongoDB database connected"))
   .catch(() => console.log("MongoDB connection failed"));
 
+//set headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 //routes
 app.use("/api/users", users);
 
--- src/authorization/signup.component.js
+++ src/authorization/signup.component.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import { Spinner, Toast } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import logo from "../shared/Components/PZ.png";
 import "./login.component.css";
 
 const Signup = () => {
+  const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const onSignupHandler = (e) => {
+    setLoading(true);
+    setShowToast(false);
+    const user = {
+      name: document.getElementById("name").value,
+      address: document.getElementById("address").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+    };
+    console.log("signup", user);
+    axios
+      .post("http://localhost:5000/api/users/signup", user)
+      .then((result) => {
+        console.log("Signup", result);
+        setLoading(false);
+        setShowToast(true);
+        setToastMessage(result.data.message);
+      })
+      .catch((error) => {
+        console.log("error signup", error);
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
       <div className="container-login100">
@@ -21,8 +51,9 @@ const Signup = () => {
                 className="input100"
                 type="text"
                 name="name"
+                id="name"
                 placeholder="Name"
-                autocomplete="off"
+                autoComplete="off"
               />
               <span className="focus-input100"></span>
               <span className="symbol-input100">
@@ -34,8 +65,9 @@ const Signup = () => {
                 className="input100"
                 type="text"
                 name="address"
+                id="address"
                 placeholder="Address"
-                autocomplete="off"
+                autoComplete="off"
               />
               <span className="focus-input100"></span>
               <span className="symbol-input100">
@@ -47,8 +79,9 @@ const Signup = () => {
                 className="input100"
                 type="text"
                 name="email"
+                id="email"
                 placeholder="Email"
-                autocomplete="off"
+                autoComplete="off"
               />
               <span className="focus-input100"></span>
               <span className="symbol-input100">
@@ -61,6 +94,7 @@ const Signup = () => {
                 className="input100"
                 type="password"
                 name="pass"
+                id="password"
                 placeholder="Password"
               />
               <span className="focus-input100"></span>
@@ -70,7 +104,13 @@ const Signup = () => {
             </div>
 
             <div className="container-login100-form-btn">
-              <button className="login100-form-btn signup-btn">Signup</button>
+              <button
+                className="login100-form-btn signup-btn"
+                onClick={(e) => onSignupHandler(e)}
+                disabled={loading}
+              >
+                {loading && <Spinner animation="border" size="sm" />} Signup
+              </button>
             </div>
             <div className="container-login100-form-btn p-t-12">
               <div className="text-center">
@@ -83,6 +123,21 @@ const Signup = () => {
               >
                 <button className="login100-form-btn">Login</button>
               </Link>
+              {showToast && (
+                <div className="notification-toast m-4">
+                  <Toast
+                    bg="Warning"
+                    className="notification-toast m-4"
+                    onClose={() => setShowToast(false)}
+                    show={showToast}
+                    autohide
+                  >
+                    <Toast.Body>
+                      <strong>{toastMessage}</strong>
+                    </Toast.Body>
+                  </Toast>
+                </div>
+              )}
             </div>
           </div>
         </div>
